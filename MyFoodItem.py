4b633f5defcf0e2b95cc8211e209a6bed53816fc
Zakk <54030858+zjwgames@users.noreply.github.com>
2022-10-01 13:22:09 +0100

    Refactored

--- MyFoodItem.py
+++
@@ -1,7 +0,0 @@
-from myfitnesspal import fooditem
-
-class MyFoodItem(fooditem.FoodItem):
-    def __init__(self, name, brand, verified, calories, grams):
-        super().__init__(self, name, brand, verified, calories)
-        self.grams = grams
-        self.quantity_needed = 0.0
